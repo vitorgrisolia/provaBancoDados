0179e8c50be34babdd928f5958632e218c099586
Vitor Grisolia <grisolia.vitor@gmail.com>
2024-04-26 23:58:28 -0300

    nav

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
 <body>
     <div>
         <nav class="nav flex-column">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
-            <a class="nav-link" href="#">Link</a>
-            <a class="nav-link" href="#">Link</a>
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <a class="nav-link active" aria-current="page" href="#">home</a>
+            <a class="nav-link" href="#">Lista de clientes</a>
+            <a class="nav-link" href="#">cadastro de clientes</a>
+            <a class="nav-link" href="#">cadastro de Pets</a>
         </nav>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
