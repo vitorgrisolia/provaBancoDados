b6402b79136e08364684e1503a65e35148d316ef
Vitor Grisolia <grisolia.vitor@gmail.com>
2024-04-29 23:35:26 -0300

    correcao no bootstrap

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">PetShopp Amigão</a>
+            <a class="navbar-brand" href="Proprietarios/home">PetShopp Amigão</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -25,7 +25,7 @@
                         <a class="nav-link" href="provaBancoDados/cadastroPet">Cadastrar Pet</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="provaBancoDados/cadastroProprietarios">Cadastrar Proprietarios</a>
+                        <a class="nav-link" href="provaBancoDados/cadastroProprietarios">Cadastrar Proprietários</a>
                     </li>
                 </ul>
             </div>
@@ -42,6 +42,7 @@
         </div>
     </footer>
 
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
